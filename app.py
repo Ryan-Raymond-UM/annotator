fa03837740b9dd2ecc564f8f13f93abb87202463
Ryan Raymond <ryanray@umich.edu>
2025-08-19 10:35:11 -0400

    Factored view (view_job) out into its own file

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ app.register_blueprint(routes.admin)
 app.register_blueprint(routes.index)
 app.register_blueprint(routes.login)
 app.register_blueprint(routes.jobs)
+app.register_blueprint(routes.view)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -243,91 +244,6 @@ def submit():
     threading.Thread(target=perform_clustering, args=(job_parameters,upload_folder, )).start()
     
     return jobs( message )
-
-@app.route('/view')
-def view_job():
-    
-    job_id = request.args.get('id')
-    job_path = f"./data/{job_id}/job_parameters.json"
-    lock_path = job_path + ".lock"
-    
-    if not os.path.exists(job_path):
-        return (
-                "Job does not exist, please check other jobs at "
-                "<a href='/jobs'>Job List</a>",
-                404
-        )
-                
-    with FileLock(lock_path):
-        with open( job_path ) as f:
-            job_parameters = json.load( f ) 
-
-    status_file_path = os.path.join( job_parameters["job_path"], 'job.status')
-    # Create a lock for the status file to avoid race conditions
-    lock_path = status_file_path + ".lock"
-    lock = FileLock(lock_path)
-    with lock:
-        with open(status_file_path, "r") as status_file:
-            job_status = json.load(status_file)
-
-    
-
-    if job_status["status"] == "completed":
-
-        # Assume clusters_dict is already generated
-        selected_cluster = request.args.get( "cluster", "0" )
-        page = int(request.args.get("page", 0))
-    
-        clusters_dict = job_parameters["clusters"]
-        
-        images_per_page = 40
-        start = page * images_per_page
-        end = start + images_per_page
-        image_paths = clusters_dict[selected_cluster][start:end]
-
-        Accuracy_Score = job_parameters["Accuracy_Score"]
-        F1_Score = job_parameters["F1_Score"]
-        labeled_data_counts = job_parameters["labeled_data_counts"]
-        checkbox_status = job_parameters.get("checkbox_status", {})
-
-
-		
-        return render_template('view_job.html', 
-                               job=job_parameters, 
-                               job_status=job_status,
-                               categories = categories, 
-							   labeled_data_counts = labeled_data_counts,
-                               accuracy_score = Accuracy_Score,
-                               f1_score = F1_Score,
-                               checkbox_status = checkbox_status,
-                               
-                               clusters_dict=clusters_dict, 
-                               selected_cluster= int(selected_cluster), 
-                               image_paths=image_paths,
-                               page=page,
-                               total_clusters=len(clusters_dict),
-                               total_images = len(clusters_dict[selected_cluster])
-                              )
-    else:
-
-        return render_template('view_job.html', 
-                               job=job_parameters, 
-                               job_status=job_status,
-                               categories = categories, 
-
-                               checkbox_status = {},
-							   labeled_data_counts = {},
-                               accuracy_score = 0,
-                               f1_score = 0,
-                               clusters_dict={}, 
-                               selected_cluster= 0, 
-                               image_paths=[],
-                               page=0,
-                               total_clusters=0,
-                               total_images = 0
-                              )
-        
-
 
 
 # @app.route('/download', methods=['GET'])
